8d9016abba94b92bcc839a912d7493024814d7ca
Nick “NeoNae” Romanov <nikitaromanov60@gmail.com>
2025-01-10 01:40:28 +0300

    Add upload form

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import path from "path";
+import fs from "fs";
+import multer from "multer";
 import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
+
+const upload = multer({ dest: "uploads/" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,5 +20,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is working on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
